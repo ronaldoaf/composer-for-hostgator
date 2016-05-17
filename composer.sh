95191577710c156def57953ebb481d0934500697
ronaldoaf <ronaldoaf>
2016-05-17 12:15:55 -0300

    Coloca o comando export para COMPOSER_HOME

--- composer.sh
+++ composer.sh
@@ -1,5 +1,5 @@
 PHP_PATH=/opt/php55/bin/php
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-COMPOSER_HOME=$DIR/
+export COMPOSER_HOME=$DIR/
 $PHP_PATH $DIR/composer.phar $@ >composer.out 2>&1 &
